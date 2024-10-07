edca2485c1b40588884807f48b38a9fbe8ad2633
sukhdevdogiyal <sukhdevdogiyal@gmail.com>
2024-10-07 21:48:15 +0530

    Add Circular Queue

--- circular_queue.c
+++ circular_queue.c
@@ -4,24 +4,24 @@ int main()
     int queue[5], rear = -1, front = -1, val, n, ch = 1, i;
     printf("Enter the size of queue : ");
     scanf("%d", &n);
-    printf("Choice:-\n1.REAR\n2.FRONT\n3.Display\n4.Exit");
+    printf("-:Choice:-\n1.Inscrtion\n2.Deletion\n3.Display\n4.Exit");
     while (ch)
     {
-        printf("\n\nEnter the choice : ");
+        printf("\nEnter the choice : ");
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
-            if (rear == n - 1 && front == 0|| rear + 1 == front)
+            if (rear == n - 1 && front == 0 || rear + 1 == front)
             {
-                printf("Queue is Full");
+                printf("Queue is Full\n");
             }
             else
             {
                 if (rear == -1)
                 {
                     front = 0;
-                    printf("Enter the element in stack : ");
+                    printf("Enter the element in Queue : ");
                     scanf("%d", &val);
                     rear += 1;
                     queue[rear] = val;
@@ -29,13 +29,13 @@ int main()
                 else if (rear == n - 1)
                 {
                     rear = 0;
-                    printf("Enter the element in stack : ");
+                    printf("Enter the element in Queue : ");
                     scanf("%d", &val);
                     queue[rear] = val;
                 }
                 else
                 {
-                    printf("Enter the element in stack : ");
+                    printf("Enter the element in Queue : ");
                     scanf("%d", &val);
                     rear += 1;
                     queue[rear] = val;
@@ -45,57 +45,70 @@ int main()
         case 2:
             if (rear == -1)
             {
-                printf("Queue is empty");
+                printf("Queue is empty\n");
             }
             else
             {
                 if (front == n - 1 && front > rear)
                 {
                     val = queue[front];
-                    printf("delete value : %d", val);
+                    printf("delete value : %d\n", val);
                     front = 0;
                 }
                 else
                 {
                     val = queue[front];
-                    printf("delete value : %d", val);
+                    printf("delete value : %d\n", val);
                     front += 1;
                 }
-                  if (rear+1==front)
+                if (rear + 1 == front)
                 {
-                    rear=-1;
-                    front=-1;
+                    rear = -1;
+                    front = -1;
                 }
-                
             }
             break;
         case 3:
             printf("Display Stack : ");
             if (rear == -1)
             {
-                printf("Queue is empty");
+                printf("Queue is empty\n");
             }
             else
             {
-                if(rear>front)
+                if (rear >= front)
                 {
-                for (i = front; i <= rear; i++)
-                {
-                    printf(" %d", queue[i]);
-                }
+                    for (i = 0; i < front; i++)
+                    {
+                        printf(" -");
+                    }
+
+                    for (i = front; i <= rear; i++)
+                    {
+                        printf(" %d", queue[i]);
+                    }
+                    for (i = rear + 1; i < n; i++)
+                    {
+                        printf(" -");
+                    }
                 }
                 else
                 {
-                for (i = 0; i <= rear; i++)
-                {
-                    printf(" %d", queue[i]);
-                }
-                    for (i = front; i <n; i++)
-                {
-                    printf(" %d", queue[i]);
-                }
+                    for (i = 0; i <= rear; i++)
+                    {
+                        printf(" %d", queue[i]);
+                    }
+                    for (i = rear + 1; i < front; i++)
+                    {
+                        printf(" -");
+                    }
 
+                    for (i = front; i < n; i++)
+                    {
+                        printf(" %d", queue[i]);
+                    }
                 }
+                printf("\n");
             }
             break;
         case 4:
@@ -104,10 +117,11 @@ int main()
             break;
 
         default:
-            printf("\nEnter valid choice");
+            printf("Enter valid choice \n");
             break;
         }
         if (ch == 4)
             break;
     }
+    return 0;
 }
